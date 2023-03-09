dced38a765b928704914136243dacbc7a29f5ad9
Muhammad Yousuf <Muhammadyousufgemini@gmail.com>
2023-03-09 15:15:43 +0500

    quiz app

--- lib/view/task2.dart
+++ lib/view/task2.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class AddPillReminder extends StatefulWidget {
   const AddPillReminder({Key? key}) : super(key: key);
 
@@ -9,146 +8,111 @@ class AddPillReminder extends StatefulWidget {
 }
 
 class _AddPillReminderState extends State<AddPillReminder> {
-  int itemcounter = 0;
   var graphbut = 1;
   var graphbut1 = 1;
   var selectedtype = 0;
   var a = 0;
-  TimeOfDay initialTime = TimeOfDay.now();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        
         body: ListView(children: [
-            profile(context),
-         
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.08,
+      profile(context),
+      SizedBox(
+        height: MediaQuery.of(context).size.height * 0.05,
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+    Icon(Icons.arrow_back_sharp,color: Colors.black,),
+    Text("DEC",style: TextStyle(fontSize: 15 ,color: Colors.black,fontWeight: FontWeight.bold),),
+    Padding(
+      padding: EdgeInsets.only(right: 10),
+      child: Text("JAN",style: TextStyle(fontSize: 15 ,color: Colors.black,fontWeight: FontWeight.bold),))
+
+      ],),
+      SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          children: [
+            InkWell(
+                onTap: () {
+                  setState(() {
+                    print(graphbut);
+                    graphbut = 1;
+                    selectedtype = 0;
+                  });
+                },
+                child: graphbut == 1
+                    ? TimesDay("14\nMon", graphbut, Colors.cyan)
+                    : TimesDay("14\nMon", graphbut, Colors.white)),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.10,
+            ),
+            InkWell(
+                onTap: () {
+                  setState(() {
+                    print(graphbut);
+
+                    graphbut = 2;
+                    selectedtype = 1;
+                  });
+                },
+                child: graphbut == 2
+                    ? TimesDay("15\nTue", graphbut, Colors.cyan)
+                    : TimesDay("15\nTue", graphbut, Colors.white)),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.10,
+            ),
+            InkWell(
+                onTap: () {
+                  setState(() {
+                    print(graphbut);
+
+                    graphbut = 3;
+                    selectedtype = 2;
+                  });
+                },
+                child: graphbut == 3
+                    ? TimesDay("16\nThu", graphbut, Colors.cyan)
+                    : TimesDay("16\nThu", graphbut, Colors.white)),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.10,
+            ),
+            InkWell(
+                onTap: () {
+                  setState(() {
+                    print(graphbut);
+
+                    graphbut = 4;
+                    selectedtype = 3;
+                  });
+                },
+                child: graphbut == 4
+                    ? TimesDay("17\nfri", graphbut, Colors.cyan)
+                    : TimesDay("17\nfri", graphbut, Colors.white)),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.10,
+            ),
+          
+          ],
+        ),
+      ),
+      SizedBox(
+        height: MediaQuery.of(context).size.height * 0.04,
+      ),
+       Padding(
+            padding: const EdgeInsets.all(14.0),
+            child: Text("Ongoing",style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black,
+                    )),
           ),
-       
-             
-               Column(children: [
-                  Container(
-                      padding: EdgeInsets.only(
-                        top: MediaQuery.of(context).size.height * 0.01,
-                        bottom: MediaQuery.of(context).size.height * 0.01,
-                        left: MediaQuery.of(context).size.height * 0.001,
-                        right: MediaQuery.of(context).size.height * 0.001,
-                      ),
-                      margin: EdgeInsets.only(
-                        top: MediaQuery.of(context).size.height * 0.001,
-                        bottom: MediaQuery.of(context).size.height * 0.001,
-                        left: MediaQuery.of(context).size.height * 0.02,
-                        right: MediaQuery.of(context).size.height * 0.02,
-                      ),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.green,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.blue.withOpacity(0.2),
-                            spreadRadius: 0.1,
-                            blurRadius: 2,
-                            offset: Offset(0, 3), // changes position of shadow
-                          ),
-                        ],
-                      ),
-                      child: Column(
-                        children: [
-                          Text(
-                            "Times a day",
-                            style: TextStyle(
-                              fontSize: 17,
-                              // letterSpacing: 1,
-                              // wordSpacing: 0.1,
-                              fontWeight: FontWeight.w400,
-                            ),
-                          ),
-                          SizedBox(
-                            height: MediaQuery.of(context).size.height * 0.01,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              SizedBox(
-                                width: MediaQuery.of(context).size.width * 0.10,
-                              ),
-                              InkWell(
-                                  onTap: () {
-                                    setState(() {
-                                      print(graphbut);
-                                      graphbut = 1;
-                                      selectedtype = 0;
-                                    });
-                                  },
-                                  child: graphbut == 1
-                                      ? TimesDay("1", graphbut,
-                                          Color.fromARGB(255, 2, 107, 6))
-                                      : TimesDay("1", graphbut, Colors.blue)),
-                              SizedBox(
-                                width: MediaQuery.of(context).size.width * 0.10,
-                              ),
-                              InkWell(
-                                  onTap: () {
-                                    setState(() {
-                                      print(graphbut);
-
-                                      graphbut = 2;
-                                      selectedtype = 1;
-                                    });
-                                  },
-                                  child: graphbut == 2
-                                      ? TimesDay("2", graphbut,
-                                          Color.fromARGB(255, 2, 107, 6))
-                                      : TimesDay("2", graphbut, Colors.blue)),
-                              SizedBox(
-                                width: MediaQuery.of(context).size.width * 0.10,
-                              ),
-                              InkWell(
-                                  onTap: () {
-                                    setState(() {
-                                      print(graphbut);
-
-                                      graphbut = 3;
-                                      selectedtype = 2;
-                                    });
-                                  },
-                                  child: graphbut == 3
-                                      ? TimesDay("3", graphbut,
-                                          Color.fromARGB(255, 2, 107, 6))
-                                      : TimesDay("3", graphbut, Colors.blue)),
-                              SizedBox(
-                                width: MediaQuery.of(context).size.width * 0.10,
-                              ),
-                              InkWell(
-                                  onTap: () {
-                                    setState(() {
-                                      print(graphbut);
-
-                                      graphbut = 4;
-                                      selectedtype = 3;
-                                    });
-                                  },
-                                  child: graphbut == 4
-                                      ? TimesDay("4", graphbut,
-                                          Color.fromARGB(255, 2, 107, 6))
-                                      : TimesDay("4", graphbut, Colors.blue)),
-                              SizedBox(
-                                width: MediaQuery.of(context).size.width * 0.10,
-                              ),
-                            ],
-                          )
-                        ],
-                      )),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.01,
-                  ),
-                
-         ] )]
-                    
-       
-        ));
+          Medicine(context,'Mobile App design','Annual kit'),
+          //  Medicine(context),
+           
+    ]));
   }
 }
 
@@ -162,17 +126,36 @@ class TimesDay extends StatelessWidget {
     var theme = Theme.of(context).textTheme;
 
     return Container(
-      height: 80,
-      padding: EdgeInsets.symmetric(horizontal: 13, vertical: 5),
-     decoration: new BoxDecoration(
-      color: Colors.white,
-      borderRadius: BorderRadius.only(
-          topRight: Radius.elliptical(
-              MediaQuery.of(context).size.width, 100.0)),
-    ),
+      height: MediaQuery.of(context).size.height * 0.17,
+      width: MediaQuery.of(context).size.height * 0.08,
+      alignment: Alignment.center,
+      margin: EdgeInsets.only(
+        top: MediaQuery.of(context).size.height * 0.03,
+        bottom: MediaQuery.of(context).size.height * 0.01,
+        left: MediaQuery.of(context).size.height * 0.01,
+        right: MediaQuery.of(context).size.height * 0.01,
+      ),
+      padding: EdgeInsets.only(
+        top: MediaQuery.of(context).size.height * 0.015,
+        left: MediaQuery.of(context).size.height * 0.01,
+        right: MediaQuery.of(context).size.height * 0.01,
+        bottom: MediaQuery.of(context).size.height * 0.01,
+      ),
+      decoration: BoxDecoration(
+        color: col,
+        borderRadius: BorderRadius.circular(30),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.2),
+            spreadRadius: 2,
+            blurRadius: 7,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
       child: Text(
         title!.toUpperCase(),
-        style: theme.bodyText2!.copyWith(color: Colors.white),
+        style: theme.bodyText2!.copyWith(color: Colors.black),
       ),
     );
   }
@@ -223,29 +206,123 @@ Future<TimeOfDay?> showTimePicker({
 }
 
 Widget profile(context) {
-    return Column(
-      children: [
-        Container(
-            child: ListTile(
-          leading: GestureDetector(
-            onTap: () {
+  return Column(children: [
+    Container(
+        child: ListTile(
+      leading: GestureDetector(
+          onTap: () {},
+          child: Icon(
+            Icons.arrow_back_ios,
+            color: Colors.black,
+          )),
+      trailing: InkWell(
+        onTap: () {},
+        child: Padding(
+          padding: const EdgeInsets.only(right: 10.0),
+          child:
+              CircleAvatar(child: ClipOval(child: Image.asset('assets/1.jpg'))),
+        ),
+      ),
+    )),
+  ]);
+}
+Widget Medicine(context,tite,subtile) {
+  return 
+      Container(
+          margin: EdgeInsets.only(
+            top: MediaQuery.of(context).size.height * 0.01,
+            bottom: MediaQuery.of(context).size.height * 0.02,
+            left: MediaQuery.of(context).size.height * 0.08,
+            right: MediaQuery.of(context).size.height * 0.02,
+          ),
+          padding: EdgeInsets.only(
+            bottom: MediaQuery.of(context).size.height * 0.02,
+            top: MediaQuery.of(context).size.height * 0.01,
+            left: MediaQuery.of(context).size.height * 0.01,
+            right: MediaQuery.of(context).size.height * 0.01,
+          ),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(25),
              
-            },
-            child: Icon(Icons.arrow_back_ios,color: Colors.black,)
-          ),
-         
-          trailing: InkWell(
-            onTap: () {
-            
-            },
-            child: Padding(
-              padding: const EdgeInsets.only(right: 10.0),
-              child:CircleAvatar(
-                child: ClipOval(child:Image.asset('assets/1.jpg'))),
-            ),
-          ),
-        )),
+              boxShadow: [
+                BoxShadow(
+                 color:Color.fromARGB(255, 196, 83, 216),
+                          offset: const Offset(
+                         1,1
+                          ),
+                        
+                        ), 
+              ]
+              
+              ),
+          child: Column(
+              
+            children: [
         
-      ]  
-    );
-  }
+              Column( children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.02,
+                ),
+                Column(children: [
+                  Row(
+                    children: [
+                      Padding(padding: EdgeInsets.only(left: 30)),
+                      Text(
+                        tite,
+                        style: TextStyle(
+                            fontSize: 21,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            letterSpacing: 2),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Padding(padding: EdgeInsets.only(left: 30)),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.05,
+                      ),
+                      Text(subtile,
+                          style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white)),
+                    ],
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.02,
+                  ),
+                  Row(
+                    children: [
+                      Padding(padding: EdgeInsets.only(left: 12)),
+                      ClipOval(
+                          child: CircleAvatar(
+                        backgroundImage: AssetImage("assets/1.jpg"),
+                      )),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.01,
+                      ),
+  Spacer(),
+                    Text("9:00am - 10:00am",style: TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white))
+                   
+                    ],
+                  ),
+                  
+  
+                ]
+                ),
+                
+              ]
+              ),
+            ],
+          
+          
+            
+          ),
+    
+  );
+}
